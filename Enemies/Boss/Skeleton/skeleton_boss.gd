05606eccb289d91b26c69516fb6a2ef770df5e5a
HiggloH <hilding.hammarback@gmail.com>
2024-05-28 11:49:07 +0200

    skeleton boss retrite and idle states

--- Enemies/Boss/Skeleton/skeleton_boss.gd
+++ Enemies/Boss/Skeleton/skeleton_boss.gd
@@ -1,21 +1,84 @@
 extends CharacterBody2D
 
+const GRAVITY: float = 1200.0
+
 enum States {
 	Idle,
 	Run,
 	Retrite,
+	Attack,
 	Attack1,
 	Attack2
 }
 
+var current_state: States
+
 @onready var attack_area_1 = $Attack1_Area
 @onready var attack_area_2 = $Attack2_Area
 
+@onready var player_retite_detection_left = $PlayerDetection/Left
+@onready var player_retite_detection_right = $PlayerDetection/Right
+
 @onready var anim_player = $AnimatedSprite2D
 
+var direction: Vector2 = Vector2.ZERO
+var player_direction: int = 0
+
+var move_speed: int = 15000
+
 func change_state(new_state: States):
+	velocity = Vector2.ZERO
+	
 	attack_area_1.monitorable = false
 	attack_area_2.monitorable = false
+	
+	current_state = new_state
+
+func start():
+	change_state(States.Idle)
+
+func _physics_process(delta):
+	match current_state:
+		States.Idle:
+			if not is_on_floor():
+				velocity.y += GRAVITY * delta
+			
+			Idle()
+		States.Retrite:
+			Retrite(delta)
+	
+	if current_state != States.Retrite:
+		if player_retite_detection_left.is_colliding() or player_retite_detection_right.is_colliding():
+			change_state(States.Retrite)
+	
+	move_and_slide()
+
+func Idle():
+	anim_player.play("idle")
+
+func Run(delta: float):
+	#Run back anbd forth randomly
+	pass
+
+func Retrite(delta: float):
+	anim_player.stop()
+	
+	#Run away from the player
+	if player_retite_detection_left.is_colliding():
+		#Run to the right
+		player_direction = 1
+	elif player_retite_detection_right.is_colliding():
+		#Run to the left
+		player_direction = -1
+	
+	velocity.x = player_direction * move_speed * delta
+	
+	if not player_retite_detection_left.is_colliding() or not player_retite_detection_right.is_colliding():
+		change_state(States.Idle)
+
+func attack(delta: float):
+	#Move to the player
+	pass
 
 func attack_2():
 	attack_area_2.monitorable = true
--- Levels/Level0-10/Boss/Skeleton_boss_manager.gd
+++ Levels/Level0-10/Boss/Skeleton_boss_manager.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 @onready var anim_player = $AnimationPlayer
+@onready var skeleton_boss = $Skeleton/SkeletonBoss
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		anim_player.play("Start")
 		
 		$Area2D.queue_free()
+		
+		await anim_player.animation_finished
+		skeleton_boss.start()
